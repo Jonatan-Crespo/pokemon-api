78b113e3a19e1f0021fdd9f8f010bca927af80ba
Jonatan Crespo <jonatan.domingues.crespo@gmail.com>
2025-05-30 15:43:41 -0300

    Logic bottons callback 20 pokemons and previous 20 pokemons

--- src/main.js
+++ src/main.js
@@ -1,31 +1,68 @@
 import './style.css'
 
-// const URL_BASE = 'https://pokeapi.co/api/v2/pokemon/';
-const URL_BASE = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0';
+let offset = 0;
+const cardContainer = document.getElementById('card');
 
-fetch(URL_BASE)
-  .then(response => response.json())
-  .then(data => {   
-    const namePokemons = Object.entries(data.results);
-   
-    namePokemons.forEach((names) => {
-      fetch(`https://pokeapi.co/api/v2/pokemon/${names[1].name}`)
-        .then(response => response.json())
-        .then(dados => {
-          const card = document.getElementById('card');
-          const div = document.createElement('div');
+// Função para carregar Pokémons
+function fetchPokemons(url) {
+  cardContainer.innerHTML = ''
 
-          const pokemonTypes = dados.types.map((tipos) => tipos.type.name);
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const namePokemons = Object.entries(data.results);
 
-          div.className = 'card';
-          div.innerHTML = `
-              <img src="${dados.sprites.other.dream_world.front_default}"
-              alt="">
+      namePokemons.forEach((names) => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${names[1].name}`)
+          .then(response => response.json())
+          .then(dados => {
+            const div = document.createElement('div');
+            const pokemonTypes = dados.types.map((tipos) => tipos.type.name);
+
+            div.className = 'card';
+            div.innerHTML = `
+              <img src="${dados.sprites.other.dream_world.front_default}" alt="">
               <h2>${dados.name}</h2>
               <p>${pokemonTypes}</p>
             `;
+            cardContainer.appendChild(div);
+          })
+          .catch(error => console.error("Erro ao carregar dados Pokémons:", error));
+      });
+    })
+    .catch(error => console.error("Erro ao carregar Pokémons:", error));
+}
 
-          card.appendChild(div);
-        })
-    });
+// botão next - logica para adicionar 20 no offset e puxar os proximos 20 pokemons
+function nextBtn() {
+  const botaoNext = document.getElementById('next');
+  botaoNext.addEventListener('click', (event) => {
+    event.preventDefault();
+    offset += 20;
+    const URL_BASE = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`;
+    fetchPokemons(URL_BASE);
+  });
+};
+
+// botão de previous - Logica para voltar 20 no offset e desativar qndo for igual a 0
+function previousBtn() {
+  const botaoPrevious = document.getElementById('previous');
+  botaoPrevious.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (offset >= 0) {
+      document.getElementById('previous').disabled = false;
+      offset -= 20;
+      const URL_BASE = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`;
+      fetchPokemons(URL_BASE);
+    }else{
+      document.getElementById('previous').disabled = true;
+    }
   })
+}
+
+fetchPokemons(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`);
+nextBtn();
+previousBtn();
+
+// // //Desbilitar o botão previous
+// // document.getElementById('previous').disabled = true;
